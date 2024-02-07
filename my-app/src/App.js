022aaf1e350f32e2079e2151e3aded575b0300c6
namexd <namexd@MacBook-Air-Suleyman.local>
2024-02-07 15:47:19 +0300

    react_11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+// function App() {
+// 	return (
+// 	<>
+// 	<div>
+// 		Flower shop
+// 		<p class="contacts">Contacts</p>
+// 		<p class="profile">My profile</p>
+// 		<ul>
+// 			<li>1</li>
+// 			<li>2</li>
+// 			<li>3</li>
+// 			<li>4</li>
+// 			<li>5</li>
+// 			<li>6</li>
+// 			<li>7</li>
+// 			<li>8</li>
+// 			<li>9</li>
+// 			<li>10</li>
+// 			</ul>
+// 			<ul>
+// 			<li>text1</li>
+// 			<li>text2</li>
+// 		</ul>
+// 	</div>
+// 	<div>
+// 		<p>text3</p>
+// 		<p>text4</p>
+// 	</div>
+// 	</>
+// 	);
+// }
+
 function App() {
-	return <div>
-		text
-	</div>;
+	return <input />;
 }
+
+// function App() {
+// 	return (
+// 		<>
+// 	<ul>
+// 		<li>text1</li>
+// 		<li>text2</li>
+// 		<li>text3</li>
+// 	</ul>
+// 	<ul>
+// 		<li>text1</li>
+// 		<li>text2</li>
+// 		<li>text3</li>
+// 	</ul>
+// 	</>
+// 	);
+// }
+
+
 
 export default App;
